ac21a257f63c4ac854ad784d46c7d89054f6bff8
Jascha Beste <bestejascha@gmail.com>
2015-09-22 10:23:12 +0200

    started filling classes with features such as the singelton Connection

--- app/src/main/java/de/szut/dqi12/texasholdem/connection/Connection.java
+++ app/src/main/java/de/szut/dqi12/texasholdem/connection/Connection.java
@@ -1,7 +1,47 @@
 package de.szut.dqi12.texasholdem.connection;
 
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  * Created by Jascha on 22.09.2015.
  */
+//This class is supposed to be used for the general Server connection. Exchange of data should be handled by send and receive.
 public class Connection {
+
+    public static String serverIP = "127.0.0.1";
+    public static int serverPort = 12345;
+    private Socket serverCon;
+    private static Connection instance;
+
+    private Connection(){
+        try {
+            serverCon = new Socket(serverIP,serverPort);
+        } catch (IOException e) {
+            //make Toast connection failed + print e. I have no Idea how to do this outside of the Main Activity tbh.
+        }
+    }
+    public static Connection getInstance(){
+        if(instance==null){
+            instance = new Connection();
+        }
+        return instance;
+    }
+
+    public boolean connect(String ip, int port){
+        try {
+            serverCon = new Socket(ip , port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        serverIP=ip;
+        serverPort=port;
+        return true;
+    }
+
+    public void disconnect(){
+        Send.sendClientAction(ClientAction.DISCONNECT);
+    }
+
 }
--- app/src/main/java/de/szut/dqi12/texasholdem/connection/Send.java
+++ app/src/main/java/de/szut/dqi12/texasholdem/connection/Send.java
@@ -4,4 +4,83 @@ package de.szut.dqi12.texasholdem.connection;
  * Created by Jascha on 22.09.2015.
  */
 public class Send {
+
+    public static boolean sendGameAction(GameAction action, String params[]){
+        switch(action) {
+            case FOLD:
+                return sendFoldAction();
+
+            case CALL:
+                return sendCallAction();
+
+            case CHECK:
+                return sendCheckAction();
+
+            case ALLIN:
+                return sendAllinAction();
+
+            case BET:
+                return sendBetAction();
+
+            case RAISE:
+                return sendRaiseAction();
+
+            case CHAT:
+                return sendChatAction(params);
+
+        }
+        return false;
+    }
+
+
+
+    public static boolean sendClientAction(ClientAction action, String params[]){
+        return false;
+    }
+
+    public static boolean sendGameAction(GameAction action){
+        return false;
+    }
+
+    public static boolean sendClientAction(ClientAction action){
+        return false;
+    }
+
+
+
+    //Just add the params[] parameter whereever it is needed I don't quite remember which action was what
+    private static boolean sendFoldAction(){
+        //// TODO: 22.09.2015  function needs to be filled
+        return false;
+    }
+
+    private static boolean sendCheckAction() {
+        //// TODO: 22.09.2015  function needs to be filled
+        return false;
+    }
+
+    private static boolean sendAllinAction() {
+        //// TODO: 22.09.2015  function needs to be filled
+        return false;
+    }
+
+    private static boolean sendCallAction() {
+        //// TODO: 22.09.2015  function needs to be filled
+        return false;
+    }
+
+    private static boolean sendBetAction() {
+        //// TODO: 22.09.2015  function needs to be filled
+        return false;
+    }
+
+    private static boolean sendRaiseAction() {
+        //// TODO: 22.09.2015  function needs to be filled
+        return false;
+    }
+
+    private static boolean sendChatAction(String params[]) {
+        //// TODO: 22.09.2015  function needs to be filled
+        return false;
+    }
 }
