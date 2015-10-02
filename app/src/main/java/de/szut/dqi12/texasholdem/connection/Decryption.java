343c77c0a0d8b6ab3d997ebbc607ebbc076240ba
Jascha Beste <bestejascha@gmail.com>
2015-10-02 17:01:33 +0200

    decrypt start of algortihm

--- app/src/main/java/de/szut/dqi12/texasholdem/connection/Decryption.java
+++ app/src/main/java/de/szut/dqi12/texasholdem/connection/Decryption.java
@@ -1,19 +1,46 @@
 package de.szut.dqi12.texasholdem.connection;
 
+import android.os.AsyncTask;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by Jascha on 22.09.2015.
  */
 public class Decryption {
 
-    private ArrayList<String> newMessages;
+    private List<String> newMessages;
 
-    public Decryption(){
+    public Decryption() {
+        newMessages = Collections.synchronizedList(new ArrayList<String>());
 
     }
+
+
+
 
     public void addNewMessage(String newMessage){
+        newMessages.add(newMessage);
+    }
 
+    private class Decrypt extends AsyncTask<List<String>, Void, Void> {
+
+        @Override
+        protected Void doInBackground(List<String>... params) {
+            List<String> newMessages = params[0];
+
+            for(String s : newMessages){
+                String[] splits  = s.split(";");
+                //String action = s.substring(0, s.indexOf(";"));
+                switch(splits[0]){
+                    case "FOLD":
+                        break;
+                }
+            }
+            return null;
+        }
     }
 }
+
