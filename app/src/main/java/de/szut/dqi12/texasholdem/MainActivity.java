d7f7a37080d0c08fff2609c54d976807c122bb89
Marcel <marcel.hofgesang@live.de>
2015-11-03 10:21:11 +0100

    xml elements got proper id's

--- app/src/main/java/de/szut/dqi12/texasholdem/MainActivity.java
+++ app/src/main/java/de/szut/dqi12/texasholdem/MainActivity.java
@@ -10,13 +10,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_menue);
+        setContentView(R.layout.startup);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        //Button nextXML = findViewById(R.id.buttonLogin);
         return true;
     }
 
