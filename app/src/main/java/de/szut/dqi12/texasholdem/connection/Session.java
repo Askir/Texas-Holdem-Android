22297aa8282c3ff7f6d5feca79172211bd394161
Askir <bestejascha@gmail.com>
2015-11-08 06:30:21 +0100

    added encryption for the password

--- app/src/main/java/de/szut/dqi12/texasholdem/connection/Session.java
+++ app/src/main/java/de/szut/dqi12/texasholdem/connection/Session.java
@@ -1,7 +1,10 @@
 package de.szut.dqi12.texasholdem.connection;
 
 import de.szut.dqi12.texasholdem.Controller;
+import de.szut.dqi12.texasholdem.action.ClientAction;
 import de.szut.dqi12.texasholdem.action.ServerAction;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by Jascha Beste on 16.10.2015.
@@ -18,7 +21,17 @@ public class Session implements Recallable {
         expectedParams = null;
     }
 
-    public void login(){
+    public void login(String username, String password){
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        messageDigest.update(password.getBytes());
+        String encryptedPassword = new String(messageDigest.digest());
+        String[] login = {username,encryptedPassword};
+        con.getSend().sendAction(ClientAction.LOGIN,login);
         expectedAction = ServerAction.LOGGEDIN;
         con.getDecryption().addExpectation(this);
 
