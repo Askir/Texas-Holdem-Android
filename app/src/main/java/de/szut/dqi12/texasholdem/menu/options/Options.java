62fc5b1c605d5bc151ee5d272894fa4cc80c4c67
Jascha Beste <bestejascha@gmail.com>
2015-12-15 08:34:57 +0100

    some more option function implementations

--- app/src/main/java/de/szut/dqi12/texasholdem/menu/options/Options.java
+++ app/src/main/java/de/szut/dqi12/texasholdem/menu/options/Options.java
@@ -1,5 +1,8 @@
 package de.szut.dqi12.texasholdem.menu.options;
 
+import de.szut.dqi12.texasholdem.Controller;
+import de.szut.dqi12.texasholdem.action.ClientAction;
+
 /**
  * Created by Jascha on 09.10.2015.
  */
@@ -22,29 +25,32 @@ public class Options {
         this.email = email;
     }
 
-    public boolean changePassword(String oldPassword, String newPassword){
-        //TODO fill function
-        return true;
+    public void changePassword(String oldPassword, String newPassword){
+        String[] params = {oldPassword,newPassword};
+        Controller.getInstance().getSend().sendAction(ClientAction.CHANGEPASSWORD, params );
+        return;
     }
-
+    /**
     public boolean forgotPassword(){
         //TODO fill function
         return true;
     }
-
+    **/
     public void setVolume(int value){
         this.volume=value;
     }
 
-    public boolean changeUsername(String username){
+    public void changeUsername(String username){
         this.username=username;
-        //TODO fill function
-        return true;
+        String[] params = {username};
+        Controller.getInstance().getSend().sendAction(ClientAction.CHANGEUSERNAME, params);
+        return;
     }
 
-    public boolean changeEmail(String email){
-        //TODO fill function
-        return true;
+    public void changeEmail(String email){
+        String[] params = {email};
+        Controller.getInstance().getSend().sendAction(ClientAction.CHANGEEMAILADDRESS,params);
+        return;
     }
 
     public void onClose(){
