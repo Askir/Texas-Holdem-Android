fe5b125091dc8b38265b2c765c44aef200428829
Askir <bestejascha@gmail.com>
2016-01-12 18:08:13 +0100

    more bullshit naming error. God I hate this shit

--- app/src/main/java/de/szut/dqi12/texasholdem/gui/joinGamePasswordtemp.java
+++ app/src/main/java/de/szut/dqi12/texasholdem/gui/joinGamePasswordtemp.java
@@ -1,42 +1,42 @@
 package de.szut.dqi12.texasholdem.gui;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import de.szut.dqi12.texasholdem.R;
 import de.szut.dqi12.texasholdem.guibackbone.GameList;
 
 /**
- * Created by Jascha on 12.01.2016.
+ * Created by Jascha Beste on 12.01.2016.
  */
 public class JoinGamePassword extends Activity {
+
+
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.join_game_password);
+
         final EditText password = (EditText) findViewById(R.id.editTextJoinGamePassword);
         Button ok = (Button) findViewById(R.id.buttonJoinGameOk);
-
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                GameList.getInstance().joinGame(GameList.getInstance().selectedLobby.lobbyID, password.getText().toString());
+                GameList.getInstance().joinGame(GameList.getInstance().selectedLobbyID, password.getText().toString());
+
             }
         });
+
     }
-
-
     public void joinGameSuccessfull(){
-        Intent lobby = new Intent(JoinGamePassword.this, CreateGameLobby.class);
-        startActivity(lobby);
 
     }
     public void joinGameFailed(String params){
-        Toast.makeText(getBaseContext(), params, Toast.LENGTH_SHORT).show();
+
     }
+
 }
